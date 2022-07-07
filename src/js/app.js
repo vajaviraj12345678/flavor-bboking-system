454c69295fd18adb498a2b7310ce2bcc829e51aa
vajaviraj12345678 <66360194+vajaviraj12345678@users.noreply.github.com>
2022-07-07 14:42:32 +0530

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import '../scss/app.scss';
+
+// import "/node_modules/bootstrap/scss/bootstrap.scss";
+/* Your JS Code goes here */
+
+/* Demo JS */
+// import './demo.js';
+
+import './index.js';
---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+import 'bootstrap';
+import 'jquery';
+
+$(document).ready(function() {
+    $('input[name="daterange"]').daterangepicker({
+      opens: 'left'
+    }, function(start, end, label) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
+
+
+
+
+
+
+const inputLeft = document.getElementById("input-left");
+const inputRight = document.getElementById("input-right");
+
+const thumbLeft = document.querySelector(".slider > .thumb.left");
+const thumbRight = document.querySelector(".slider > .thumb.right");
+const range = document.querySelector(".slider > .range");
+
+const setLeftValue = () => {
+  const _this = inputLeft;
+  const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+  _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+  const percent = ((_this.value - min) / (max - min)) * 100;
+  thumbLeft.style.left = percent + "%";
+  range.style.left = percent + "%";
+};
+
+const setRightValue = () => {
+  const _this = inputRight;
+  const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+  _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+  const percent = ((_this.value - min) / (max - min)) * 100;
+  thumbRight.style.right = 100 - percent + "%";
+  range.style.right = 100 - percent + "%";
+};
+
+inputLeft.addEventListener("input", setLeftValue);
+inputRight.addEventListener("input", setRightValue);
